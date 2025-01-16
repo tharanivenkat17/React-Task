867669379d5a78b3f01dc120d732ed6db49f4f3f
Sivatharani Venkat <sivatharani.venkat@aspiresys.com>
2025-01-16 14:33:48 +0530

    Yarn

---
+++ file/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import HelloWorld from './HelloWorld.jsx'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <HelloWorld></HelloWorld>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ file/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HelloWorld</h1>
+      </div>
+    )
+  }
+}
+
+export default HelloWorld
