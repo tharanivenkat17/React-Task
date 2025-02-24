4ba426ced1fb0f9d7ffdfcb4393a54a3f1bb4fd3
Sivatharani Venkat <sivatharani.venkat@aspiresys.com>
2025-02-24 09:23:04 +0530

    Performance Optimization

--- taskapp/src/AdvanceTask/Performance/Performance.jsx
+++ taskapp/src/AdvanceTask/Performance/Performance.jsx
@@ -2,35 +2,38 @@ import React, { useCallback, useMemo, useState } from 'react'
 
 function Performance() {
     const [count, setCount] = useState(0);
-    const [number, setNumber] = useState(0);
+    const [count1, setCount1] = useState(0);
+    const memoizedValue = useMemo(() => {
+        console.log('Memoizing count:', count);
+        return count * 2;
+    }, [count]);
+    console.log("Rendering component");
 
-    const double = useMemo(() => {
-        return number + 1;
-    }, [number]);
-
-    const increse = useCallback(() => {
-        setNumber((prev) => prev + 1)
-    }, []);
+    const [countcall, setCountcall] = useState(0);
+    const handleClick = useCallback(() => {
+        setCountcall(countcall + 1);
+    }, [countcall]);
 
     return (
         <div>
             <h3>Performance Optimization</h3>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount(count + 1)}>Add</button>
-            <MemoComponent number={double} onClick={increse} />
-            <h4>Double the Value: {double}</h4>
-            <button onClick={increse}>Increase</button>
+            <h4>Use Memo</h4>
+            <button onClick={() => setCount(count + 1)}>Increment Count</button>
+            <p>Count With Memo: {count}</p>
+            <p>Memoized value (count * 2): {memoizedValue}</p>
+            <button onClick={() => setCount1(count1 + 1)}>Increment Count Without Memo</button>
+            <p>Count Without Memo: {count1}</p>
+
+            <h4>Use Callback</h4>
+            <Child handleClick={handleClick} />
+            <p>Count: {countcall}</p>
         </div>
     )
 }
 
 export default Performance
 
-const MemoComponent = React.memo(({ number, onClick }) => {
-    return (
-        <div>
-            <h4>Number : {number}</h4>
-            <button onClick={onClick}>Increase</button>
-        </div>
-    )
-})
+function Child({ handleClick }) {
+    console.log('Child rendered');
+    return <button onClick={handleClick}>Increase Count</button>;
+}
